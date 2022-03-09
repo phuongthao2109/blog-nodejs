92af4960274746349133a395a50fafc4dd647687
thaotp-98 <81809411+thaotp-98@users.noreply.github.com>
2022-03-09 11:11:16 +0700

    feat: deleted count documment

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -6,9 +6,24 @@ class AdminController {
         res.render('admin/dashboard')
     }
     listCourses(req, res, next) {
-        Course.find({})
-            .then(courses => { res.render('admin/Course/list', { courses: mutipleMongooseToObject(courses) }) })
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deleteCount]) => {
+                res.render('admin/Course/list', {
+                    deleteCount: deleteCount,
+                    courses: mutipleMongooseToObject(courses)
+                })
+            })
             .catch(next)
+
+
+        //lấy tất cả các bản ghi đã xóa
+        // Course.countDocumentsDeleted()
+        // .then(deleteCount =>{
+        //     console.log('deleteCount', deleteCount)
+        // }) 
+        // Course.find({})
+        //     .then(courses => { res.render('admin/Course/list', { courses: mutipleMongooseToObject(courses) }) })
+        //     .catch(next)
     }
     create(req, res, next) {
         res.render('admin/Course/create')
@@ -22,35 +37,37 @@ class AdminController {
 
     getOldData(req, res, next) {
         Course.findById(req.params.id)
-        .then(oldcourse => { res.render('admin/Course/edit',{
-            oldcourse : mongooseToObject(oldcourse) 
-        })})
-        .catch(next)
+            .then(oldcourse => {
+                res.render('admin/Course/edit', {
+                    oldcourse: mongooseToObject(oldcourse)
+                })
+            })
+            .catch(next)
     }
     edit(req, res, next) {
-        Course.updateOne({_id: req.params.id},req.body)
-        .then(() => res.redirect('/admin/courses'))
-        .catch(next)
+        Course.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect('/admin/courses'))
+            .catch(next)
     }
     delete(req, res, next) {
-        Course.delete({_id: req.params.id})
-        .then(() => res.redirect('back'))
-        .catch(next)
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next)
     }
     courseBind(req, res, next) {
         Course.findDeleted({})
-        .then(courses => { res.render('admin/Course/bin', { courses: mutipleMongooseToObject(courses) }) })
-        .catch(next) 
+            .then(courses => { res.render('admin/Course/bin', { courses: mutipleMongooseToObject(courses) }) })
+            .catch(next)
     }
-    RestorecourseBind(req, res, next){
-        Course.restore({_id: req.params.id})
-        .then(() => res.redirect('back'))
-        .catch(next)
+    RestorecourseBind(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next)
     }
-    forceDelete(req, res, next){
-        Course.deleteOne({_id: req.params.id})
-        .then(() => res.redirect('back'))
-        .catch(next)
+    forceDelete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next)
     }
 }
 
